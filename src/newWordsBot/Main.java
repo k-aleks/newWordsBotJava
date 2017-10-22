d3f085635978ce422f38ebd20e812013362ba746
Aleksandr Kazakov <kazakoa@amazon.com>
2017-10-22 13:20:17 +0100

    Configured logging

--- src/newWordsBot/Main.java
+++ src/newWordsBot/Main.java
@@ -10,8 +10,12 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        for (int i=0; i< 10;i++)
+        for (int i=0; i< 5;i++) {
+            logger.debug("test logging " + i);
             logger.info("test logging " + i);
+            logger.warn("test logging " + i);
+            logger.error("test logging " + i);
+        }
     }
 }
 
