a7d1c070f384ba2674951bf4f95e0e2d1142333a
Aleksandr Kazakov <kazakoa@amazon.com>
2017-10-22 12:49:58 +0100

    Tuning log4j2

--- src/newWordsBot/Main.java
+++ src/newWordsBot/Main.java
@@ -1,11 +1,17 @@
 package newWordsBot;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 import java.util.Date;
 import java.util.UUID;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-	// write your code here
+        for (int i=0; i< 10;i++)
+            logger.info("test logging " + i);
     }
 }
 
